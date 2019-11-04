50170c978f4a5d25ebfe7f774e0b4d84b757d53c
Romy van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2019-11-04 13:30:50 +0100

    inintial commit

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
 import os
-from flask import Flask
+from datetime import datetime  #This will import a timestamp
+from flask import Flask, redirect, render_template, request, session
 
+"""
+Create a (temp) session-cookie, which will store our username so that when we visit the website, 
+it will redirect us to our personal homepage. Add 'request' and 'session' to the `import`-part above.
+The session-cookie will cease to exist once the browser has been totally closed off.
+"""
 app = Flask(__name__)
+app.secret_key = "randonstrgin123" #Generally in production, this will be set as an environment variable like an IP address import.
+messages = []
 
-@app.route('/')
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S") #The strftime() method takes a date/time object and converts it into a string according to a given format.
+    
+    """Create dictionary to store message information(data) as key-value pairs."""
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    """ timestamp will store the value of `now` (%H:%M:%S)
+        from will store the value username, and thus the username of the user
+        message will store the value message, which will be the written text on screen
+    """
+    messages.append(messages_dict) #append the whole messages dictionary.
+
+
+@app.route('/', methods = ["GET","POST"]) #Create these methods to make "GET" and "POST" active, in correspondance with index.html
 def index():
-    return "To send a message use /USERNAME/MESSAGE"
+
+    if request.method == "POST": #If request.method is equal to "POST" then create a new var in session called ["username"].
+        session["username"] = request.form["username"] #The ["username"] of session must be equal to that of the request.form.
+
+    if "username" in session: #If the username here and in session are the same, then redirect to the session username, taking us to this route.
+        return redirect (session["username"]) 
+
+    return render_template("index.html")
 """Main page with instructions"""
 
 @app.route("/<username>") #This way, it will get treated as a variable.
 def user(username): #This function is going to bind to our root decorator.
-    return "Hi" + username
+    """Display chat message"""
+    return render_template("chat.html", username= username, chat_messages= messages)
 
 @app.route("/<username>" + "/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)  
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)  
 
 #app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
